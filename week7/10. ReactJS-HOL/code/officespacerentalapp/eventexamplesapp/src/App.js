08ab491004d94eae91083b51549821f44ea507ab
SRIHITHA0 <138369032+SRIHITHA0@users.noreply.github.com>
2025-08-03 21:33:22 +0530

    adding week7

---
+++ week7/10. ReactJS-HOL/code/officespacerentalapp/eventexamplesapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EventExamples from './Components/EventExamples';
+import CurrencyConvertor from './Components/CurrencyConvertor';
+
+function App() {
+  return (
+    <div className="App">
+      <EventExamples />
+      <CurrencyConvertor />
+    </div>
+  );
+}
+
+export default App;
